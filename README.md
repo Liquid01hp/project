d7638c997044962be5da9ece2c34d36a87213aec
Liquid01hp <valdezjenner99@gmail.com>
2024-02-18 10:57:44 -0500

    First Push

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project
+
+The following scatterplot and histogram represent the coorelation according to Odometer and Price so show how some cars
+will sell regardless of the miles and if the price is right. They could be old cars that are worth a lot in value.
---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+csv_file_path = r'C:\Users\valde\Desktop\Sprint_6\project\vehicles_us.csv'
+
+# Read the dataset
+df = pd.read_csv(csv_file_path)
+
+# At least one st.header with text
+st.header("First Project")
+
+# At least one Plotly Express histogram
+hist_column = st.selectbox("fuel", df.columns)
+fig_hist = px.histogram(df, x=hist_column, title=f'Histogram of {hist_column}')
+st.plotly_chart(fig_hist)
+
+# At least one Plotly Express scatter plot
+scatter_x = st.selectbox("odometer", df.columns)
+scatter_y = st.selectbox("price", df.columns)
+fig_scatter = px.scatter(df, x=scatter_x, y=scatter_y, title='Odometer and Price correlation')
+st.plotly_chart(fig_scatter)
+
+# At least one checkbox to change the behavior
+show_details = st.checkbox("Show details")
+if show_details:
+    st.write("Additional details can be displayed here.")

6aa4acf4bf22e45afc6da0ea106c62e05d160ff1
Liquid01hp <valdezjenner99@gmail.com>
2024-02-21 18:56:00 -0500

    fixed names and descriptions

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ csv_file_path = 'vehicles_us.csv'
 # Read the dataset
 df = pd.read_csv(csv_file_path)
 
-#Prints out the number of duplicates
-(df.duplicated().sum())
-df = df.drop_duplicates()
-
 # At least one st.header with text
 st.header("Vehicles Information")
 st.subheader("The following information will give you a better understanding about the cars, "
@@ -25,7 +21,7 @@ fig_hist = px.histogram(df, x=hist_column, title=f'Histogram of {hist_column}')
 st.plotly_chart(fig_hist)
 
 # Description of histogram
-st.markdown("Most vehicles are gas with around 47,000 and counting making that 91%\ of the overal number of vehicles, followed by Diesel with about 3,700 that being 7.1%, leaving electric as the lowest %\ out of all the cars being 0.01 due to electric cars only being 6 of them in total.")
+st.markdown("Most vehicles are gas with around 47,000 and counting making that 91% of the overal number of vehicles, followed by Diesel with about 3,700 that being 7.1%, leaving electric as the lowest % out of all the cars being 0.01 due to electric cars only being 6 of them in total.")
 
 #Default values for x and y axes in scatter plot
 default_x = 'Odometer'
@@ -38,7 +34,7 @@ fig_scatter = px.scatter(df, x=scatter_x, y=scatter_y, title='Odometer and Price
 st.plotly_chart(fig_scatter)
 
 #Description of scatterplot
-st.text("The expected trend of odometer and price seems to be accurate, the price of the vehicle declines as the odometer level increases, for exception of some cars which could be classic cars or electric cars that often aren't affect by the amount of miles.")
+st.markdown("The expected trend of odometer and price seems to be accurate, the price of the vehicle declines as the odometer level increases, for exception of some cars which could be classic cars or electric cars that often aren't affect by the amount of miles.")
 
 # At least one checkbox to change the behavior
 show_details = st.checkbox("Show details")

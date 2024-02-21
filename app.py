6f7547a7a231d3984d519cf2eb85500411ddb91d
Liquid01hp <valdezjenner99@gmail.com>
2024-02-20 21:05:16 -0500

    test 1

--- app.py
+++ app.py
@@ -23,12 +23,9 @@ fig_hist = px.histogram(df, x=hist_column, title=f'Histogram of {hist_column}')
 st.plotly_chart(fig_hist)
 
 # Description of histogram
-description_hist  = """Most vehicles are gas with around 47,000 and counting making that 91% of the overal number of vehicles, 
-followed by Diesel with about 3,700 that being 7.1%, leaving electric as the lowest % out of all the cars being 0.01
-due to electric cars only being 6 of them in total.
-"""
-print(description_hist)
-
+st.text = ("Most vehicles are gas with around 47,000 and counting making that 91% of the overal number of vehicles,"
+"followed by Diesel with about 3,700 that being 7.1%, leaving electric as the lowest % out of all the cars being 0.01"
+"due to electric cars only being 6 of them in total.")
 
 #Default values for x and y axes in scatter plot
 default_x = 'odometer'
@@ -43,11 +40,9 @@ fig_scatter.update_layout(xaxis_title_text='Model Year')
 st.plotly_chart(fig_scatter)
 
 #Description of scatterplot
-description_scat  ="""The expected trend of odometer and price seems to be accurate, 
-the price of the vehicle declines as the odometer level increases, for exception of some cars 
-which could be classic cars or electric cars that often aren't affect by the amount of miles.
-"""
-print(description_scat)
+st.text =("The expected trend of odometer and price seems to be accurate,"
+"the price of the vehicle declines as the odometer level increases, for exception of some cars"
+"which could be classic cars or electric cars that often aren't affect by the amount of miles.")
 
 # At least one checkbox to change the behavior
 show_details = st.checkbox("Show details")
